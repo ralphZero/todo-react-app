548f9987321cd0817adfdb4b4cfcb68d06aca164
ralphZero <placideralph@gmail.com>
2022-08-15 19:58:25 -0400

    working on pageHeader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Layout, PageHeader, Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons'
 
+import AddCommentForm from './components/AddCommentForm';
 import './App.css';
+
+const { Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Layout className='container'>
+        <PageHeader
+          className="page-header"
+          title="TO DO APP"
+          subTitle="Yep! Another one"
+          extra={[
+            <Avatar icon={<UserOutlined />} />
+          ]}
+        />
+      <Content style={{
+        padding: '0 50px',
+      }}>
+        Main Content
+      </Content>
+      <Footer className="bg-warning">
+        <AddCommentForm />
+      </Footer>
+    </Layout>
   );
 }
 
