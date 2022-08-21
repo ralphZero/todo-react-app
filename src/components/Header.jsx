e7af7da777dc5e14b407245283e39823c282885d
ralphZero <placideralph@gmail.com>
2022-08-21 17:13:51 -0400

    get data by user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,10 +10,12 @@ const Header = () => {
     <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%' }} className="page-header">
       <div>Todo App</div>
       <Space size={10}>
-        <AddButton />
         {
           user ? (
-            <Button onClick={logOut} size="large" shape="round" type="text">{user.displayName}<Avatar style={{ marginLeft: 5, marginBottom: 2 }} size="small" src={user.photoURL} /></Button>
+            <>
+              <AddButton />
+              <Button onClick={logOut} size="large" shape="round" type="text">{user.displayName}<Avatar style={{ marginLeft: 5, marginBottom: 2 }} size="small" src={user.photoURL} /></Button>
+            </>
           ) : (
             <Button onClick={logIn} size="large" shape="round" type="text">Login</Button>
           )
--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, createContext } from 'react';
+import React, { useEffect, useState, createContext, useContext } from 'react';
+import { UserContext } from './UserContext';
 
 export const DataContext = createContext();
 
@@ -6,12 +7,16 @@ const DataContextProvider = (props) => {
 
     const [dataList, setDataList] = useState();
 
+    const { user } = useContext(UserContext);
+
     useEffect(() => {
-        fetch('https://us-central1-todo-app-rsp.cloudfunctions.net/todo')
-        .then(res => res.json())
-        .then(data => setDataList(data))
-        .catch(console.error)
-    }, [])
+        if(user) {
+            fetch('https://us-central1-todo-app-rsp.cloudfunctions.net/todo?u='+user.id)
+            .then(res => res.json())
+            .then(data => setDataList(data))
+            .catch(console.error)
+        }
+    }, [user])
 
     const addToList = (data) => {
         const tempList = [...dataList];
